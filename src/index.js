7689cfe7002a54c933671767871bfb8c0e9579e1
zyphorixx <24256@iiitu.ac.in>
2026-01-05 15:53:16 +0530

    Added cron-job for scheduling reminders

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const sendBasicEmail = require('./services/email-services');
 
+const cron = require('node-cron');
+
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
@@ -12,12 +14,16 @@ const setupAndStartServer = () => {
     app.listen(3004, () => {
         console.log(`Server started on port ${PORT}`);
 
-        sendBasicEmail(
-            'support@admin.com',
-            'itsrajsingh56@gmail.com',
-            'This is a testing email',
-            'Hey, how are you, I hope you like the support'
-        );
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'itsrajsingh56@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, how are you, I hope you like the support'
+        // );
+
+        cron.schedule('* * * * *', () => {
+           console.log('Runs every minute');
+        });
     });
 }
 
